634b0039ce0abc4b938f4dc3822a6d43e91167ed
Ciaowilly3 <muscomassi@gmail.com>
2022-12-20 15:25:13 +0100

    filtro parcheggio

--- index.php
+++ index.php
@@ -38,6 +38,27 @@
         ],
 
     ];
+    $filteredHotels = $hotels;
+
+    if(isset($_GET['parkRequired'])){
+        $parkFilter = $_GET['parkRequired'];
+        $filteredHotels = [];
+        if ($parkFilter == 'true'){
+            foreach ($hotels as $hotel) {
+                if($hotel['parking']){
+                    $filteredHotels[] = $hotel;
+                }    
+            }
+        }else{
+            foreach ($hotels as $hotel) {
+                if(!$hotel['parking']){
+                    $filteredHotels[] = $hotel;
+                }    
+            }
+        }
+    }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="it">
@@ -49,12 +70,27 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- <link rel="stylesheet" href="css/style.css"> -->
-        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <!-- <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> -->
     </head>
     <body class="bg-dark">
         <div class="container">
-            <h1 class="text-center text-primary mb-5">HOTEL? <del>TriVaGo</del> Boolean! </h1>
+            <h1 class="text-center text-primary my-5">HOTEL? <del>TriVaGo</del> Boolean! </h1>
+            <form method="GET" class="mb-3 border border-secondary p-3">
+                <div class="mb-3 text-white">
+                    <div class="w-50">
+                        <h5>Necessiti del parcheggio?</h5>
+                        <input type="radio" name="parkRequired" value="true" id="yes">
+                        <label for="yes" class="me-3">Yes</label>
+                        <input type="radio" name="parkRequired" value="false" id="no">
+                        <label for="no">No</label>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-primary btn-lg me-3">Invia</button>
+                    <button class="btn btn-warning btn-lg" href="#">Annulla</button>
+                </div>
+            </form>
             <table class="table text-white">
                 <thead>
                     <tr>
@@ -66,7 +102,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($hotels as $hotel) {
+                <?php foreach ($filteredHotels as $hotel) {
                 
                 ?>
                 <tr>
@@ -91,6 +127,5 @@
                 </tbody>
             </table>
         </div>
-        <script src="js/main.js"></script>
     </body>
 </html>
